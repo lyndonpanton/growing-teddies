07029e6136ada440d25da05e0568ee9d12e97c88
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-02 20:01:10 +0000

    Added initial unity project

---
+++ Exercise11/Assets/Scripts/YellowTeddyBear.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script for the Yellow Teddy Bear unity sprite
+/// </summary>
+
+public class YellowTeddyBear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 newScale = transform.localScale;
+
+        newScale.x *= 4;
+        newScale.y *= 4;
+    }
+}
